2024e860bd0152e7d3abfc7306400c7e85859f43
colbertt <thomas.finckel@suncoastfcu.org>
2018-12-24 14:05:16 -0500

    Add project files.

---
+++ SCU.Onboarding/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SCU.Onboarding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
